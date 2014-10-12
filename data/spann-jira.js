39e8129fec1eb4e74892db8a4fae5e341149d4a0
Sunil Kumar <sunil.rebel@gmail.com>
2014-10-12 23:52:08 +0530

    Modified spann-jira.js & added tab to open

--- data/spann-jira.js
+++ data/spann-jira.js
@@ -4,4 +4,14 @@
 
 $(function(){
 
-})
+    setTimeout(function(){
+        process()
+    }, 10000)
+
+});
+
+
+function process() {
+    var str = $(document).xpath(".//*[@id='issuetable']/tbody/tr/td[3]").html();
+    alert(str)
+}
