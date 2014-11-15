8d446087fa7f2426172bcdaeb02940df1ea1f24d
Sunil Kumar <sunil.rebel@gmail.com>
2014-11-16 01:52:45 +0530

    [2] Added main panel with basic input tag

---
+++ data/main-panel.js
@@ -0,0 +1,8 @@
+/**
+ * Created by sunil on 16-11-2014.
+ */
+
+var extractButton = document.getElementById("extractButton");
+extractButton.addEventListener('click', function onclick(event) {
+    console.log("extract button clicked")
+});
