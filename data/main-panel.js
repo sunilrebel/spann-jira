9f312064be1c8936fe66b4be7f0fad9c474eec3d
Sunil Kumar <sunil.rebel@gmail.com>
2014-11-16 02:19:02 +0530

    [2] Flowing user input url to jiraListExtractor

--- data/main-panel.js
+++ data/main-panel.js
@@ -2,7 +2,11 @@
  * Created by sunil on 16-11-2014.
  */
 
-var extractButton = document.getElementById("extractButton");
+var extractButton = document.querySelector("#extractButton");
+var userInput = document.querySelector("#userInput");
+
 extractButton.addEventListener('click', function onclick(event) {
-    console.log("extract button clicked")
+    var input = userInput.value;
+    input = "https://jira.mongodb.org/issues/?jql=text%20~%20%22mongo%22";
+    self.port.emit("extractData", input);
 });
